5b896f1cea7aea3ffd76d0abebc3115b0bd53704
Nurbek Abduraximov <84277066+Nurbek21@users.noreply.github.com>
2022-02-15 17:22:30 +0500

    changed by nurbek (#9)
    Co-authored-by: Tokhirbek <98032035+Tokhirbek7@users.noreply.github.com>

--- lazizbek/src/main/java/uz/pdp/Main.java
+++ lazizbek/src/main/java/uz/pdp/Main.java
@@ -2,7 +2,7 @@ package uz.pdp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("lazizasdasdbek");
+        System.out.println("Contributed by Nurbek");
     }
 
 
