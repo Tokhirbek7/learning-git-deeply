e6e45e8b06515210d6357ba4a4cc5a2da5275f2f
Lazizbek12 <tulanov.laziz2000@gmail.com>
2022-02-15 16:59:33 +0500

    changed at 4:59

--- lazizbek/src/main/java/uz/pdp/Main.java
+++ lazizbek/src/main/java/uz/pdp/Main.java
@@ -2,7 +2,7 @@ package uz.pdp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("lazizbek");
+        System.out.println("lazizasdasdbek");
     }
 
 
