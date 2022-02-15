08c7c7c59748aeb7a4e031f545dceabb533b8769
Lazizbek12 <96458387+Lazizbek12@users.noreply.github.com>
2022-02-15 17:01:33 +0500

    Lazizbek (#8)
    * changed by Lazizbek
    
    * changed at 4:59

--- lazizbek/src/main/java/uz/pdp/Main.java
+++ lazizbek/src/main/java/uz/pdp/Main.java
@@ -2,6 +2,8 @@ package uz.pdp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("tokhirbek");
+        System.out.println("lazizasdasdbek");
     }
+
+
 }
