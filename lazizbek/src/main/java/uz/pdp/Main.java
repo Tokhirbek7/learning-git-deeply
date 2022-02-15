d91bc5c7b0dc5783b921379022ff786900a3f4b7
Nurbek21 <abduraximovnurbek0002@gmail.com>
2022-02-15 17:03:29 +0500

    changed by nurbek

--- lazizbek/src/main/java/uz/pdp/Main.java
+++ lazizbek/src/main/java/uz/pdp/Main.java
@@ -2,6 +2,7 @@ package uz.pdp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("tokhirbek");
+        System.out.println("Contributed by Nurbek");
+
     }
 }
